2e661945df4eebbd585a833387b8451c176fa27a
3h5a9 <syedmdehsan@outlook.com>
2024-02-12 11:51:20 +0000

    resize featured image in index page

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ApplicationRecord
   belongs_to :category
-  has_one_attached :featured_image
+  has_one_attached :featured_image do |attachable|
+    attachable.variant :thumb, resize_to_fill: [900, 250]
+  end
   has_rich_text :content
   belongs_to :user
   has_many :comments, dependent: :destroy
